fec7191cbd7957f2b7c72349d8523827273c38d2
Slava Plisko <pliskovs@gmail.com>
2013-05-14 21:27:44 +0300

    Fix test

--- tests/Sample/FooTest.php
+++ tests/Sample/FooTest.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: amdy
- * Date: 5/14/13
- * Time: 8:37 PM
- * To change this template use File | Settings | File Templates.
- */
-
-class FooTest extends PHPUnit_Framework_TestCase {
+class FooTest extends PHPUnit_Framework_TestCase
+{
     public function testFoo()
     {
         $this->assertTrue(true);
